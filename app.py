ee06372a7ab7890256d4798ee1cebf7cea4343a3
Ubuntu <ubuntu@ip-172-31-30-175.ec2.internal>
2025-08-29 05:54:28 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+FROM flask import Flask
+app = Flask(__name__)
+
+@app.route("/")                                                                                                                                             def home():
+    return "Hello from Flask App - Deployed via Jenkins CI/CD Pipeline!"                                                                                                                                                                                                                                                @app.route("/health")                                                                                                                                       def health():
+    return {"status": "UP"}, 200                                                                                                                                                                                                                                                                                        if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ test/test_app.py
@@ -0,0 +1,19 @@
+import unittest
+from app import app
+
+class FlaskAppTest(unittest.TestCase):
+    def setup(self):
+        self.app = app.test_client()
+
+    def test_home(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Hello from Flask App", response.data)
+
+    def test_health(self):
+        response = self.app.get("/health")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"UP", response.data)
+
+if __name__ == "__main__":
+    unittest.main()
