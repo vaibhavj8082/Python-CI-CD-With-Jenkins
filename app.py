afb71e2829801a7f82ccbcc6936c108f8d5a350b
Ubuntu <ubuntu@ip-172-31-30-175.ec2.internal>
2025-08-29 06:32:23 +0000

    Updated File

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-FROM flask import Flask
+from flask import Flask
+
 app = Flask(__name__)
 
-@app.route("/")                                                                                                                                             def home():
-    return "Hello from Flask App - Deployed via Jenkins CI/CD Pipeline!"                                                                                                                                                                                                                                                @app.route("/health")                                                                                                                                       def health():
-    return {"status": "UP"}, 200                                                                                                                                                                                                                                                                                        if __name__ == "__main__":
+@app.route("/")
+def home():
+    return "Hello from Flask App - Deployed via Jenkins CI/CD Pipeline!"
+
+@app.route("/health")
+def health():
+    return {"status": "UP"}, 200
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
