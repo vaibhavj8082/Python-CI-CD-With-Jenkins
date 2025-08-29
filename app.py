68ea1367e7a08067e3c017806dd65af0b7801acd
Ubuntu <ubuntu@ip-172-31-30-175.ec2.internal>
2025-08-29 06:54:36 +0000

    Updated Files

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask App - Deployed via Jenkins CI/CD Pipeline!"
+    return render_template("index.html")
 
 @app.route("/health")
 def health():
